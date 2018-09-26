dcc494f3995d577f0b45d42a652a7e8012f8776b
Matheus Faustino <matheuspfaustino@gmail.com>
2018-09-26 14:03:51 -0300

    Remove threads reference

--- start_crawler.php
+++ start_crawler.php
@@ -1,20 +1,6 @@
 <?php
 
-require __DIR__ . '/vendor/autoload.php';
-
-
+require __DIR__.'/vendor/autoload.php';
 
 // without thread
-//(new \Phpcrawler\ProcessorPoolRequest(new \Phpcrawler\SpiderStackOverflow()))->run();
-
-// with thread
-if (extension_loaded('pthreads')) {
-    // multithread, dahora. Problema agora é que tem coisa que pode ser multithread e coisas que nao.
-    // a fila nao pode e tbm o starts_url eu creio que nao, ou na vdd, só a parte do request e da funcao do cara pode
-    $pool = new \Pool(2, \Auto\AutoloaderComposerWorker::class, [__DIR__ . '/vendor/autoload.php']);
-
-    $pool->submit(new \Phpcrawler\ProcessThread(new \Phpcrawler\SpiderStackOverflow()));
-    $pool->submit(new \Phpcrawler\ProcessThread(new \Phpcrawler\SpiderStackOverflow()));
-
-    $pool->shutdown();
-}
+(new \Phpcrawler\ProcessorPoolRequest(new \Phpcrawler\SpiderStackOverflow()))->run();
